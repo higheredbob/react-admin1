35c0537a4781eee71074c5699d97a5d2c7aff6dd
Rob FunGentoo <higheredbob@gmail.com>
2019-01-30 15:05:01 -0700

    Add files via upload

---
+++ precache-manifest.807af8d11e534690c6c2af14b5c80653.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/static/media/shape.208f3d38.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d564ecc8da899af19aff90f9bbf8ecaa",
+    "url": "/static/media/blog-post.3.d564ecc8.md"
+  },
+  {
+    "revision": "8c6ad8d34fb944fedb72c536f63ea434",
+    "url": "/static/media/blog-post.2.8c6ad8d3.md"
+  },
+  {
+    "revision": "9dbfaeb9bd1beabd55a57a27e003dd5a",
+    "url": "/static/media/blog-post.1.9dbfaeb9.md"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86c2a4e0cd08e7d8cd1d",
+    "url": "/static/js/main.86c2a4e0.chunk.js"
+  },
+  {
+    "revision": "6c8b3882dc7c9ec86b23",
+    "url": "/static/js/1.6c8b3882.chunk.js"
+  },
+  {
+    "revision": "86c2a4e0cd08e7d8cd1d",
+    "url": "/static/css/main.38358759.chunk.css"
+  },
+  {
+    "revision": "59242aef924752cf89c8e6070f222db9",
+    "url": "/index.html"
+  }
+];
